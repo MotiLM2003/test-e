3e1d518ec4b7ab46a45ab986191e1e50e6a05f90
Moti Elmakies <motiphone2003@gmail.com>
2021-10-15 02:18:41 +0300

    asds

--- pages/ProducetFeed.tsx
+++ pages/ProducetFeed.tsx
@@ -16,11 +16,48 @@ const ProductDetails = () => {
           <div>
             <Image
               src="/ProductFeeds/main.svg"
-              height="500`"
-              width="500"
+              height="500"
+              width="570"
               objectFit="contain"
               alt="test"
             />
+            <div className="flex gap-3 mb-3 justify-center">
+              <Image
+                src="/small-image.svg"
+                height="90"
+                width="90"
+                objectFit="contain"
+                alt="test"
+              />
+              <Image
+                src="/small-image.svg"
+                height="90"
+                width="90"
+                objectFit="contain"
+                alt="test"
+              />
+              <Image
+                src="/small-image.svg"
+                height="90"
+                width="90"
+                objectFit="contain"
+                alt="test"
+              />
+              <Image
+                src="/small-image.svg"
+                height="90"
+                width="90"
+                objectFit="contain"
+                alt="test"
+              />
+              <Image
+                src="/small-image.svg"
+                height="90"
+                width="90"
+                objectFit="contain"
+                alt="test"
+              />
+            </div>
           </div>
           <div className="max-w-lg text-lg">
             <h1>
@@ -81,8 +118,33 @@ const ProductDetails = () => {
                 alt="test"
               />
             </div>
+            <div></div>
           </div>
         </div>
+        <div className="max-w-screen-2xl mx-auto text-lg">
+          <div className="flex items-center  ml-24 mt-4 gap-10 text-2xl text-gray-400 border-b-4 mb-5 w-full">
+            <span className="text-black"> Product Details</span>
+            <span>Product Details</span>
+          </div>
+          <p className=" ml-24 text-sm text-gray-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque tincidunt elementum laoreet. Sed sollicitudin malesuada
+            rhoncus. Nam in dictum quam, quis accumsan nunc. Donec facilisis
+            blandit massa lacinia euismod. Sed dignissim tempus nisl sit amet
+            pharetra. In lobortis urna sed sem lobortis tristique. Nullam
+            aliquet mi sit amet augue malesuada viverra. Nam molestie in turpis
+            sed vehicula. Suspendisse potenti.
+          </p>
+        </div>
+      </section>
+      <section className="footer bg-main-light  text-black mb-10 mt-5 p-10 flex flex-col  items-center">
+        <h1 className="text-2xl">
+          Our extension is available for your browser!
+        </h1>
+        <h2 className="text-sm text-gray-400 mt-2">
+          {' '}
+          our Adjusted Rating without having to leave Amazon
+        </h2>
       </section>
       <Footer />
     </div>
